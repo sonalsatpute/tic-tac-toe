d320a8318cf603889172dfb12ba577d025a06815
Sonal Satpute <sonal.satpute@gmail.com>
2015-03-01 22:59:54 +0530

    Board created

---
+++ TicTacToe/TicTacToe.Business/Game.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe.Business
+{
+    public class Game
+    {
+        readonly int[,] _board;
+
+        public Game(int boadSize)
+        {
+            _board = new int[boadSize,boadSize];
+        }
+
+        public int GetRowCount()
+        {
+            return _board.GetLength(0);
+        }
+
+        public int GetColumnCount()
+        {
+            return _board.GetLength(1);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.Specifications/nothing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Machine.Specifications;
+using TicTacToe.Business;
+
+namespace TicTacToe.Specifications
+{
+    [Subject("Tic Tac Toe")]
+    public class TicTacToeBoardSpecs
+    {
+        public class When_given_boad_size
+        {
+            static Game _game;
+
+            Establish context = () => { _game = new Game(3); };
+
+            It should_have_3_rows = () => _game.GetRowCount().ShouldEqual(3);
+            It should_have_3_columns = () => _game.GetColumnCount().ShouldEqual(3);
+        }
+    }
+}
