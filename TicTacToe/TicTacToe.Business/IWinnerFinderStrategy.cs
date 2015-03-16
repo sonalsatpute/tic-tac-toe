9c63e011ad21923dbb03e7af36c5a25cc6b7d174
Sonal Satpute <sonal.satpute@gmail.com>
2015-03-16 23:18:18 +0530

    Move files to class

--- TicTacToe/TicTacToe.Business/IWinnerFinderStrategy.cs
+++ TicTacToe/TicTacToe.Business/IWinnerFinderStrategy.cs
@@ -4,49 +4,4 @@ namespace TicTacToe.Business
     {
         CellState FindWinner(Board board);
     }
-
-    public class ByColumnWinnerFinderStrategy : IWinnerFinderStrategy
-    {
-        Board _board;
-
-        public CellState FindWinner(Board board)
-        {
-            _board = board;
-
-            return GetWinnnerByColumn();
-        }
-
-
-        public CellState GetWinnnerByColumn()
-        {
-            CellState columnCellState = CellState.Empty;
-
-            for (int column = 0; column < Board.BOARD_SIZE; column++)
-            {
-                columnCellState = ColumnCellState(column);
-
-                if (columnCellState != CellState.Empty)
-                    return columnCellState;
-            }
-
-            return columnCellState;
-        }
-
-
-
-        private CellState ColumnCellState(int column)
-        {
-            CellState firstCellState  = _board.GetCellStatus(0, column);
-            CellState secondCellState = _board.GetCellStatus(1, column);
-            CellState thirdCellState  = _board.GetCellStatus(2, column);
-
-            bool firsSecondAndThirdColumnCellHasSameCellState = firstCellState == secondCellState &&
-                                                          firstCellState == thirdCellState;
-
-            if (firstCellState != CellState.Empty && firsSecondAndThirdColumnCellHasSameCellState)
-                return firstCellState;
-
-            return CellState.Empty;
-        }
-    }
 }
